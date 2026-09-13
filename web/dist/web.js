ac2bbee7d799ad351eb5a3fb6c4cb0344fb77cc6
Li Fengmin <2080291162@qq.com>
2026-09-13 11:44:06 +0800

    build: refresh web artifacts

--- web/dist/web.js
+++ web/dist/web.js
@@ -1062,11 +1062,11 @@ function _M0MP270717lee10pixelforge5Image5canny(self, low, high) {
   }
   return out;
 }
-function _M0MP270717lee10pixelforge5Image8contrastN5applyS2813(factor, c) {
+function _M0MP270717lee10pixelforge5Image8contrastN5applyS2819(factor, c) {
   return _M0MPC16double6Double7to__int((c + 0 - 128) * factor + 128);
 }
 function _M0MP270717lee10pixelforge5Image8contrast(self, factor) {
-  return _M0MP270717lee10pixelforge5Image8map__rgb(self, (r, g, b) => ({ _0: _M0MP270717lee10pixelforge5Image8contrastN5applyS2813(factor, r), _1: _M0MP270717lee10pixelforge5Image8contrastN5applyS2813(factor, g), _2: _M0MP270717lee10pixelforge5Image8contrastN5applyS2813(factor, b) }));
+  return _M0MP270717lee10pixelforge5Image8map__rgb(self, (r, g, b) => ({ _0: _M0MP270717lee10pixelforge5Image8contrastN5applyS2819(factor, r), _1: _M0MP270717lee10pixelforge5Image8contrastN5applyS2819(factor, g), _2: _M0MP270717lee10pixelforge5Image8contrastN5applyS2819(factor, b) }));
 }
 function _M0FP270717lee10pixelforge7diffuse(buf, w, h, x, y, err) {
   if ((x + 1 | 0) < w) {
